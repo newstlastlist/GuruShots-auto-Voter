84985ca4fe0f202605b1f104554ec86b5a59307a
mjohl <mjohl.inox@gmail.com>
2020-09-03 07:56:23 +0100

    Update of GuruShitsAutoLike.js
    This is a fix version for the swap problem that will ignore the modal buttons for swaps. Tested and works 100%.

--- GuruShitsAutoLike.js
+++ GuruShitsAutoLike.js
@@ -1,15 +1,19 @@
 const puppeteer = require('puppeteer');
 
 async function main() {
-  //const browser = await puppeteer.launch();
-   const browser = await puppeteer.launch({headless: true});
+   //const browser = await puppeteer.launch();
+   const browser = await puppeteer.launch({
+      headless: false,
+      dumpio: false,
+      devtools: false
+   });
 
    const page = await browser.newPage();
-   await page.setViewport({width: 1200, height: 720})
+   await page.setViewport({ width: 1200, height: 720 })
    await page.goto('https://gurushots.com/', { waitUntil: 'networkidle0' });
 
    //__________________ sign in _____________________________________
-   const loginBtn = '#header > gs-header > div > div > a.signin';   
+   const loginBtn = '#header > gs-header > div > div > a.signin';
    const login = '#dialogContent_0 > md-dialog-content > form > div:nth-child(1) > input';
    const password = '#dialogContent_0 > md-dialog-content > form > div:nth-child(2) > input';
    const loginSubmit = '#dialogContent_0 > md-dialog-content > form > button'
@@ -30,66 +34,68 @@ async function main() {
 
    await page.waitForNavigation();
    //await page.evaluate(() => console.log('2222'));
-  //_______________________________________________________________________
-   await page.waitFor(7000);
-   await page.evaluate(async ()  => {      
-      const voteBtns = document.getElementsByClassName('icon-vote-negative');    
-      const boostBtns =  document.getElementsByClassName('boost-state-available');       
-      console.log(voteBtns.length);
-      
+   //_______________________________________________________________________
+   await page.waitFor(4000);
+   await page.evaluate(async () => {
+      const voteBtns = document.getElementsByClassName('icon-vote-negative');
+      const boostBtns = document.getElementsByClassName('boost-state-available');
+      //console.log(voteBtns.length);
+
       //vote for photos
-      for (var btn of voteBtns){ 
-                
-         $(btn).click();         
-         await new Promise(resolve => setTimeout(resolve, 7000));         
-         const picForVote = $(".modal-vote__photo__vote");         
+      for (var btn of voteBtns) {
 
-         if(picForVote.length === 0){
-            $('div.modal-vote__submit.on').click();
-            await new Promise(resolve => setTimeout(resolve, 7000)); 
-            $('div.gs-btn--blue').click();
-            await new Promise(resolve => setTimeout(resolve, 7000));
-            continue;
+         //Only click button if the first class is for exposure vote
+         if (btn.parentNode.classList[0] == "c-challenges-item-mobile__exposure__vote") {
+            $(btn).click();
+            await new Promise(resolve => setTimeout(resolve, 4000));
+            const picForVote = $(".modal-vote__photo__vote");
 
-         } else {
-            //await picForVote.each((i, el) => $(el).click());
-            await picForVote.each(function(i, el) {
-               r = Math.random();
-               if(i >= 20 && r > 0.2)
-               {
-                  $(el).click();
-               }
-            });
-            await new Promise(resolve => setTimeout(resolve, 7000));            
-            $('div.modal-vote__submit.on').click();
-            await new Promise(resolve => setTimeout(resolve, 7000));   
-            $('div.modal-vote__btn').click();
-            await new Promise(resolve => setTimeout(resolve, 7000)); 
-            
+            if (picForVote.length === 0) {
+               $('div.modal-vote__submit.on').click();
+               await new Promise(resolve => setTimeout(resolve, 4000));
+               $('div.gs-btn--blue').click();
+               await new Promise(resolve => setTimeout(resolve, 4000));
+               continue;
+
+            } else {
+               //await picForVote.each((i, el) => $(el).click());
+               await picForVote.each(function (i, el) {
+                  r = Math.random();
+                  if (i >= 20 && r > 0.2) {
+                     $(el).click();
+                  }
+               });
+               await new Promise(resolve => setTimeout(resolve, 4000));
+               $('div.modal-vote__submit.on').click();
+               await new Promise(resolve => setTimeout(resolve, 4000));
+               $('div.modal-vote__btn').click();
+               await new Promise(resolve => setTimeout(resolve, 4000));
+
+            }
          }
       }
 
       // try to boost
-      if(boostBtns.length >= 1){
-         for (var btn of boostBtns){ 
+      if (boostBtns.length >= 1) {
+         for (var btn of boostBtns) {
 
-            $(btn).click(); 
-            await new Promise(resolve => setTimeout(resolve, 7000));
-            const picForVote = document.querySelector('div.c-modal-boost__photos > div:nth-child(1)'); 
-            $(picForVote).click(); 
-            await new Promise(resolve => setTimeout(resolve, 7000));
-            
-         }    
-      } 
-      
+            $(btn).click();
+            await new Promise(resolve => setTimeout(resolve, 4000));
+            const picForVote = document.querySelector('div.c-modal-boost__photos > div:nth-child(1)');
+            $(picForVote).click();
+            await new Promise(resolve => setTimeout(resolve, 4000));
 
-      
+         }
+      }
+
+
+
    });
 
-  
-  
-  
-  await browser.close();
+
+
+
+   await browser.close();
 }
 
 
